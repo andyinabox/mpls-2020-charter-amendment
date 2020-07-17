bcb6e85183259f11c5c243cc0e97184be9445983
Andy Dayton <amdayton@gmail.com>
2020-07-17 00:04:28 -0500

    Add csv data

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const glob = require('glob-fs')();
 const pdf = require('pdf-parse');
 const Sentiment = require('sentiment');
+const { Parser } = require('json2csv');
 
 const dateRe = /\d+\/\d+\/\d+ \d+:\d+:\d+ [AP]M\s+/g
 const sentiment = new Sentiment()
@@ -24,7 +25,7 @@ const getCombinedText = async (files) => {
 }
 
 
-const parse = async (corpus) => {
+const parseData = async (corpus) => {
   const data = []
   const responses = corpus.split(dateRe)
   // remove first empty string
@@ -34,8 +35,10 @@ const parse = async (corpus) => {
   while ((matches = dateRe.exec(corpus)) !== null) {
     const text = responses.shift().trim().split("\n").join('')
     data.push({
-      date: matches[0],
+      timestamp: matches[0].trim(),
       sentiment: sentiment.analyze(text).score,
+      position: "",
+      notes: "",
       text,
     })
   }
@@ -44,10 +47,12 @@ const parse = async (corpus) => {
 }
 
 const run = async () => {
+  const csvParser = new Parser()
   const files = glob.readdirSync("data/*.pdf")
   const corpus = await getCombinedText(files)
-  const responses = await parse(corpus);
+  const responses = await parseData(corpus);
   fs.writeFileSync("data.json", JSON.stringify(responses, null, 2));
+  fs.writeFileSync("data.csv", csvParser.parse(responses))
 }
 
 run();
